c6a23a63b40fe35b374b07ca5bd6ce202348bc72
pilkul <pilkul.energydrink@gmail.com>
2017-08-15 10:35:52 +0900

    FirstCommit
    最初のコミットです

---
+++ Assets/Unitychan.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unitychan : MonoBehaviour {
+
+	private Animator animator;
+	private CapsuleCollider CC;
+	private Rigidbody rb;
+	public float MaxSpeed = 0.01f;
+	private float speed = 0.1f;
+	private bool jumpflag;
+
+
+	void Start () {
+		animator = GetComponent<Animator>();
+		CC = GetComponent<CapsuleCollider> ();
+		rb = GetComponent<Rigidbody> ();
+	}
+		
+
+	void Update () {
+		transform.rotation = Quaternion.Euler (0, 0, 0);
+
+		float ver;
+		if (jumpflag) {
+			ver = 1;
+		} else {
+			speed = 0.1f;
+			ver = Input.GetAxis ("Vertical");
+		}
+		float hor = Input.GetAxis("Horizontal");
+		if (ver > 0) {
+			animator.SetBool ("isRunning", true);
+			transform.position += transform.forward * speed;
+
+			if (hor > 0) {
+				transform.position += new Vector3 (0.03f, 0, 0);
+			}else if(hor < 0) {
+				transform.position += new Vector3 (-0.03f, 0, 0);
+			}
+
+		} else if (ver < 0) {
+			animator.SetBool ("isWalkback", true);
+			transform.position -= transform.forward * 0.01f;
+		} else {
+			animator.SetBool ("isRunning", false);
+			animator.SetBool ("isWalkback", false);
+		}
+
+
+
+		//ジャンプの処理
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			animator.SetBool ("isJump", true);
+			StartCoroutine ("JumpAction");
+		} else {
+			animator.SetBool ("isJump", false);
+		}
+
+
+
+		//スライディングの処理	
+		if (Input.GetKeyDown (KeyCode.Z)) {
+			animator.SetBool ("isSliding", true);
+		} else {
+			animator.SetBool ("isSliding", false);
+		}
+	}
+
+	IEnumerator JumpAction(){
+		jumpflag = true;
+		rb.useGravity = false;
+		speed = 0.1f;
+		yield return new WaitForSeconds (1.2f);
+		speed = 0;
+		yield return new WaitForSeconds (0.5f);
+		rb.useGravity = true;
+		jumpflag = false;
+	}
+}
---
+++ Assets/block.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class block : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	//2.7 -3.8
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3 (speed, 0, 0);
+		if (transform.position.x > 2.7f || transform.position.x < -3.8f) {
+			speed *= -1;
+		}
+	}
+}
